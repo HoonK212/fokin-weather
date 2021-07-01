7993860664a5f0ee0b8bb6e899774d3ff37b1110
HoonK <b0aeiou@gmail.com>
2021-07-01 19:06:18 +0900

    React Native ToyProject Complete

--- App.js
+++ App.js
@@ -1,21 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import { Alert } from 'react-native';
+import Loading from './Loading';
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "6f9e3a966e460e6814c8c1945d8a0c06";
+
+// export default function App() {
+//   return <Loading />;
+// }
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp
+    });
+  };
+
+  getLocation = async () => {
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
+    }
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.yellowView}>
+//         <Text>Hi</Text>
+//       </View>
+//       <View style={styles.blueView}>
+//         <Text>Hello</Text>
+//       </View>
+//       {/* <StatusBar style="auto" /> */}
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     // backgroundColor: '#fff',
+//     // alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   yellowView: {
+//     flex: 2,
+//     backgroundColor: "yellow",
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   },
+//   blueView: {
+//     flex: 3,
+//     backgroundColor: "blue",
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   }
+// });
